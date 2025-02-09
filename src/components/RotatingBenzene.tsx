f9d6ec6ef65104d14adaf7fae93ef1e5a76728b7
Will Jay <will@rothbright.com>
2025-02-09 16:18:37 -0700

    Refactored and updated the readme

--- src/components/RotatingBenzene.tsx
+++ src/components/RotatingBenzene.tsx
@@ -4,8 +4,8 @@ const RotatingBenzene: React.FC = () => {
   return (
     <div className="flex justify-center items-center">
       <motion.svg
-        width="200"
-        height="200"
+        width="50"
+        height="50"
         viewBox="0 0 100 100"
         xmlns="http://www.w3.org/2000/svg"
         animate={{ rotate: 360 }}
@@ -14,10 +14,10 @@ const RotatingBenzene: React.FC = () => {
         <polygon
           points="50,20 75,35 75,65 50,80 25,65 25,35"
           fill="none"
-          stroke="black"
+          stroke="white"
           strokeWidth="2"
         />
-        <circle cx="50" cy="50" r="15" fill="none" stroke="black" strokeWidth="2" />
+        <circle cx="50" cy="50" r="15" fill="none" stroke="white" strokeWidth="2" />
       </motion.svg>
     </div>
   );
--- src/components/organisms/NavigationMenu.tsx
+++ src/components/organisms/NavigationMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import Link from "next/link";
 import StyledHamburgerButton from "../ui/Buttons/StyledHamburgerButton";
 import Overlay from "../ui/Overlay/Overlay";
 
@@ -14,34 +15,32 @@ const NavigationMenu: React.FC = () => {
   return (
     <div>
       <Overlay className={`overlay ${isOpen ? 'fixed' : 'hidden'}`} onClick={() => setIsOpen(false)} />
-      <StyledHamburgerButton onClick={toggleMenu} aria-label="Toggle Navigation Menu" />
+      <StyledHamburgerButton onClick={toggleMenu} aria-label="Toggle Navigation Menu" className="h-full d-flex items-center" />
 
       <div
-        className={`fixed top-0 right-0 w-64 h-full bg-gray-800 text-white transition-transform duration-300 ${
+        className={`fixed top-0 right-0 w-128 h-full bg-gray-800 text-white transition-transform duration-300 ${
           isOpen ? "transform translate-x-0" : "transform translate-x-full"
         }`}
         style={{ zIndex: 100 }}
       >
-        <ul className="space-y-4 p-6">
+        <ul className="space-y-4 p-6 text-base">
           <li>
-            <a href="#home" onClick={() => setIsOpen(false)}>
-              Home
-            </a>
+            <Link href="/about" onClick={() => setIsOpen(false)}>About</Link>
           </li>
           <li>
-            <a href="#about" onClick={() => setIsOpen(false)}>
-              About Us
-            </a>
+            <Link href="/resume" onClick={() => setIsOpen(false)}>Resume</Link>
           </li>
           <li>
-            <a href="#services" onClick={() => setIsOpen(false)}>
-              Services
-            </a>
+            <Link href="/projects/demo" onClick={() => setIsOpen(false)}>Demo Pages</Link>
           </li>
           <li>
-            <a href="#contact" onClick={() => setIsOpen(false)}>
-              Contact
-            </a>
+            <Link href="/projects/code-challenge" onClick={() => setIsOpen(false)}>Code Challenges</Link>
+          </li>
+          <li>
+            <Link href="/projects/snippets" onClick={() => setIsOpen(false)}>Snippets</Link>
+          </li>
+          <li>
+            <Link href="/contact" onClick={() => setIsOpen(false)}>Contact</Link>
           </li>
         </ul>
       </div>
